8db9e8095df35d4bc576f08812b1aa935d37fa9e
vrtem <vrtem@github.com>
2022-02-09 18:37:27 +0100

    Token validation and creation

--- src/main/java/com/artvasiliuk/qrgenerator/services/TokenService.java
+++ src/main/java/com/artvasiliuk/qrgenerator/services/TokenService.java
@@ -3,16 +3,25 @@ package com.artvasiliuk.qrgenerator.services;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class TokenService {
 
     private int tokenLength = 7;
+    private List<String> tokenList;
 
     public String createToken() {
-        return RandomStringUtils.randomAlphanumeric(tokenLength).toUpperCase();
+        String token = RandomStringUtils.randomAlphanumeric(tokenLength).toUpperCase();
+        tokenList.add(token);
+        return token;
     }
 
     public boolean validateToken(String token) {
-        return true;
+        if (tokenList.contains(token)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }

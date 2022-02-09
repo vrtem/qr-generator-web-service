516e8b38142d5ab0a04d5eb3f48cbdec5a7601bd
vrtem <vrtem@github.com>
2022-02-09 17:59:10 +0100

    Project init

---
+++ src/main/java/com/artvasiliuk/qrgenerator/QrgeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.artvasiliuk.qrgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrgeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrgeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/artvasiliuk/qrgenerator/QrgeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artvasiliuk.qrgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrgeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
